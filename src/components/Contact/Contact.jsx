7b93d1e675d53ee2943125eac5a7d7f67a822273
Karthik-GR <karthikgr525@gmail.com>
2024-07-10 12:26:50 +0530

    third

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -20,17 +20,19 @@ export const Contact = () => {
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/in/grkarthik/">
+          <a href="https://www.linkedin.com/in/grkarthik/" target="_blank">
             linkedin.com/grkarthik
           </a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://github.com/Karthik-GR">github.com/Karthik-GR</a>
+          <a href="https://github.com/Karthik-GR" target="_blank">
+            github.com/Karthik-GR
+          </a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/leetcode.png")} alt="Leetcode icon" />
-          <a href="https://leetcode.com/u/Karthik-G_R/">
+          <a href="https://leetcode.com/u/Karthik-G_R/" target="_blank">
             leetcode.com/Karthik-G_R
           </a>
         </li>
